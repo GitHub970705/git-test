70ef1e8808d5667bc50791e2cb32fdf283d764ed
zhiqiang <zhiqiang@djj.com>
2022-10-15 00:04:14 +0800

    push commit

--- src/main/java/com/atdjj/git/GitTest.java
+++ src/main/java/com/atdjj/git/GitTest.java
@@ -8,5 +8,8 @@ public class GitTest {
         System.out.println("hello gi4");
         System.out.println("master test!");
         System.out.println("hot-fix test");
+        System.out.println("push test");
+
+
     }
 }
